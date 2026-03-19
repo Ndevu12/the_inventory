e36894c5c6a0ac4e9b58f821c560bb5ee779d045
Ndevu12 <niyokwizerwajeanpaulelisa@email.com>
2026-03-19 16:04:35 +0000

    fix(cors) fixing cors issues

--- the_inventory/settings/base.py
+++ the_inventory/settings/base.py
@@ -252,6 +252,18 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "x-tenant",  # Custom header for multi-tenant support
+]
 
 
 # drf-spectacular (OpenAPI)
